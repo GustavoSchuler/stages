526ffe9b65bd05d55ba13639abf392b8ed121fbe
Luis Gustavo Bier Schuler <luis.schuler@gmail.com>
2021-09-02 18:38:47 -0300

    refactor: changing file sctructure

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+import { default as Parent } from './parent/parent.component';
+import { default as Child } from './child/child.component';
+
+export {
+  Parent,
+  Child,
+}
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Parent } from './components';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <React.StrictMode>
+    <Parent />
+  </React.StrictMode>,
+  rootElement
+);
